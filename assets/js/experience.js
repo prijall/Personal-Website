5c72fb697b82176c7a7b8f62f81831962600b6e5
prijall <khadkaprijal@gmail.com>
2024-06-09 16:53:43 +0545

    Update

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -25,6 +25,13 @@ const exp = [
     time: "(Dec, 19 - Jan, 20)",
     desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
   },
+  {
+    title: "Research Intern",
+    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
+    place: "IIT, Bombay",
+    time: "(Dec, 19 - Jan, 20)",
+    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+  },
 ];
 
 const showCards2 = () => {
